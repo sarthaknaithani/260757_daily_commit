1cf532bdc2476b39f01ac13357766869eb86ad05
Sarthak Naithani <sarthaknaithani@yahoo.in>
2021-04-18 21:39:15 +0530

    Create basic_operators.py

---
+++ basic_operators.py
@@ -0,0 +1,32 @@
+a = 21
+b = 10
+c = 0
+c = a + b
+print (" Value of c is ", c)
+c += a
+print (" Value of c is ", c )
+c *= a
+print (" Value of c is ", c )
+c /= a
+print (" Value of c is ", c )
+c = 2
+c %= a
+print (" Value of c is ", c)
+c **= a
+print (" Value of c is ", c)
+c //= a
+print (" Value of c is ", c)
+##BINARY OPERATORS
+print ('a=',a,':',bin(a),'b=',b,':',bin(b))
+c = a & b; 
+print ("result of AND is ", c,':',bin(c))
+c = a | b; 
+print ("result of OR is ", c,':',bin(c))
+c = a ^ b; 
+print ("result of EXOR is ", c,':',bin(c))
+c = ~a; 
+print ("result of COMPLEMENT is ", c,':',bin(c))
+c = a << 2; 
+print ("result of LEFT SHIFT is ", c,':',bin(c))
+c = a >> 2; 
+print ("result of RIGHT SHIFT is ", c,':',bin(c))
